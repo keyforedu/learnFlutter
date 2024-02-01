491a4ed87833110187f67d1b9270e905835f8e3c
keyforedu <keyforedu@gmail.com>
2024-02-01 10:34:40 +0700

    post session 58

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,5 +1,7 @@
+import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
+// import 'package:adv_basics/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,6 +13,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen(){
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -22,7 +32,7 @@ class _QuizState extends State<Quiz> {
               Color.fromARGB(255, 107, 15, 168),
             ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
